2f9b620d4b983e1cf9de3d8853593b7c4ef58627
martin-wilkinson <martinwilkinson645@gmail.com>
2023-12-03 19:07:53 +1100

    Push before first test

--- Assets/Scripts/QRCode.cs
+++ Assets/Scripts/QRCode.cs
@@ -1,18 +1,109 @@
-using System.Collections;
-using System.Collections.Generic;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.OpenXR;
+using Microsoft.MixedReality.QR;
+using TMPro;
 using UnityEngine;
 
-public class QRCode : MonoBehaviour
+namespace Microsoft.MixedReality.QuestMRTK3
 {
-    // Start is called before the first frame update
-    void Start()
+    public class QRCode : MonoBehaviour
     {
-        
-    }
+        public Microsoft.MixedReality.QR.QRCode qrCode;
+        private GameObject qrCodeCube;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public float PhysicalSize { get; private set; }
+        public string CodeText { get; private set; }
+
+        private TextMeshPro QRID;
+        private TextMeshPro QRNodeID;
+        private TextMeshPro QRText;
+        private TextMeshPro QRVersion;
+        private TextMeshPro QRTimeStamp;
+        private TextMeshPro QRSize;
+        private GameObject QRInfo;
+        private bool validURI = false;
+        private bool launch = false;
+        private System.Uri uriResult;
+        private long lastTimeStamp = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            PhysicalSize = 0.1f;
+            CodeText = "Dummy";
+            if (qrCode == null)
+            {
+                throw new System.Exception("QR Code Empty");
+            }
+
+            PhysicalSize = qrCode.PhysicalSideLength;
+            CodeText = qrCode.Data;
+
+            qrCodeCube = gameObject.transform.Find("Cube").gameObject;
+            QRInfo = gameObject.transform.Find("QRInfo").gameObject;
+            QRID = QRInfo.transform.Find("QRID").gameObject.GetComponent<TextMeshPro>();
+            QRNodeID = QRInfo.transform.Find("QRNodeID").gameObject.GetComponent<TextMeshPro>();
+            QRText = QRInfo.transform.Find("QRText").gameObject.GetComponent<TextMeshPro>();
+            QRVersion = QRInfo.transform.Find("QRVersion").gameObject.GetComponent<TextMeshPro>();
+            QRTimeStamp = QRInfo.transform.Find("QRTimeStamp").gameObject.GetComponent<TextMeshPro>();
+            QRSize = QRInfo.transform.Find("QRSize").gameObject.GetComponent<TextMeshPro>();
+
+            QRID.text = "Id:" + qrCode.Id.ToString();
+            QRNodeID.text = "NodeId:" + qrCode.SpatialGraphNodeId.ToString();
+            QRText.text = CodeText;
+
+            if (System.Uri.TryCreate(CodeText, System.UriKind.Absolute, out uriResult))
+            {
+                validURI = true;
+                QRText.color = Color.blue;
+            }
+
+            QRVersion.text = "Ver: " + qrCode.Version;
+            QRSize.text = "Size:" + qrCode.PhysicalSideLength.ToString("F04") + "m";
+            QRTimeStamp.text = "Time:" + qrCode.LastDetectedTime.ToString("MM/dd/yyyy HH:mm:ss.fff");
+            QRTimeStamp.color = Color.yellow;
+            Debug.Log("Id= " + qrCode.Id + "NodeId= " + qrCode.SpatialGraphNodeId + " PhysicalSize = " + PhysicalSize + " TimeStamp = " + qrCode.SystemRelativeLastDetectedTime.Ticks + " QRVersion = " + qrCode.Version + " QRData = " + CodeText);
+        }
+
+        void UpdatePropertiesDisplay()
+        {
+            // Update properties that change
+            if (qrCode != null && lastTimeStamp != qrCode.SystemRelativeLastDetectedTime.Ticks)
+            {
+                QRSize.text = "Size:" + qrCode.PhysicalSideLength.ToString("F04") + "m";
+
+                QRTimeStamp.text = "Time:" + qrCode.LastDetectedTime.ToString("MM/dd/yyyy HH:mm:ss.fff");
+                QRTimeStamp.color = QRTimeStamp.color == Color.yellow ? Color.white : Color.yellow;
+                PhysicalSize = qrCode.PhysicalSideLength;
+                Debug.Log("Id= " + qrCode.Id + "NodeId= " + qrCode.SpatialGraphNodeId + " PhysicalSize = " + PhysicalSize + " TimeStamp = " + qrCode.SystemRelativeLastDetectedTime.Ticks + " Time = " + qrCode.LastDetectedTime.ToString("MM/dd/yyyy HH:mm:ss.fff"));
+
+                qrCodeCube.transform.localPosition = new Vector3(PhysicalSize / 2.0f, PhysicalSize / 2.0f, 0.0f);
+                qrCodeCube.transform.localScale = new Vector3(PhysicalSize, PhysicalSize, 0.005f);
+                lastTimeStamp = qrCode.SystemRelativeLastDetectedTime.Ticks;
+                QRInfo.transform.localScale = new Vector3(PhysicalSize / 0.2f, PhysicalSize / 0.2f, PhysicalSize / 0.2f);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdatePropertiesDisplay();
+            if (launch)
+            {
+                launch = false;
+                LaunchUri();
+            }
+        }
+
+        void LaunchUri()
+        {
+#if WINDOWS_UWP
+            // Launch the URI
+            UnityEngine.WSA.Launcher.LaunchUri(uriResult.ToString(), true);
+#endif
+        }
     }
 }
